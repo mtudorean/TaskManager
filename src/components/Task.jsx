7b41a6f75f494194e365b4949dcac95c77f63504
mtudorean <tudoreanmuhamed7@gmail.com>
2026-09-18 09:17:09 +0300

    Actualizare Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,1 +1,11 @@
+function Task({ task }) {
+  return (
+    <li>
+      <input type="checkbox" checked={task.completed} readOnly />
+      <span>{task.title}</span>
+      <button>Șterge</button>
+    </li>
+  );
+}
 
+export default Task;
