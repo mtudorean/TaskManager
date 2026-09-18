3d7100072d314329dd7c0bf5ea8f76afb84e3f79
mtudorean <tudoreanmuhamed7@gmail.com>
2026-09-18 09:41:20 +0300

    Am adaugat o nouă stare filter visibleTasks pe baza ei

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Task from "./components/Task";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("all"); 
 
   function addTask(title) {
     const newTask = { id: Date.now(), title, completed: false };
@@ -21,22 +22,35 @@ function App() {
   function deleteTask(id) {
     setTasks((prev) => prev.filter((task) => task.id !== id));
   }
+
   const totalCount = tasks.length;
   const completedCount = tasks.filter((task) => task.completed).length;
+
+  const visibleTasks = tasks.filter((task) => {
+    if (filter === "active") return !task.completed;
+    if (filter === "completed") return task.completed;
+    return true; // "all"
+  });
 
   return (
     <main>
       <h1>Task Manager</h1>
       <p>Total sarcini: {totalCount}</p>
       <p>Finalizate: {completedCount}</p>
-      
+
       <TaskForm onAddTask={addTask} />
+
+      <div>
+        <button onClick={() => setFilter("all")}>Toate</button>
+        <button onClick={() => setFilter("active")}>Active</button>
+        <button onClick={() => setFilter("completed")}>Finalizate</button>
+      </div>
 
       {tasks.length === 0 ? (
         <p>Nu există sarcini momentan.</p>
       ) : (
         <ul>
-          {tasks.map((task) => (
+          {visibleTasks.map((task) => (
             <Task
               key={task.id}
               task={task}
